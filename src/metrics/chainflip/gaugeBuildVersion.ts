6108392d034592a09d502e6702080814fccd1065
Thomas Burton <tomjohnburton@gmail.com>
2023-11-28 16:41:31 +0100

    fix: schema and lint

--- src/metrics/chainflip/gaugeBuildVersion.ts
+++ src/metrics/chainflip/gaugeBuildVersion.ts
@@ -1,35 +1,34 @@
-import promClient, { Gauge } from "prom-client";
-import { Context } from "../../lib/interfaces";
+import promClient, {Gauge} from "prom-client";
+import {Context} from "../../lib/interfaces";
 import makeRpcRequest from "../../utils/makeRpcRequest"
 
 const metricName: string = "cf_build_version";
 const metric: Gauge = new promClient.Gauge({
-  name: metricName,
-  help: "Current version of the runtime and node.",
-  registers: [],
-  labelNames: ["runtime", "node"],
+    name: metricName,
+    help: "Current version of the runtime and node.",
+    registers: [],
+    labelNames: ["runtime", "node"],
 });
 
 export const gaugeBuildVersion = async (context: Context): Promise<void> => {
-  const { logger, api, registry } = context;
+    const {logger, api, registry} = context;
 
-  logger.debug(`Scraping ${metricName}`);
+    logger.debug(`Scraping ${metricName}`);
 
-  if (registry.getSingleMetric(metricName) === undefined)
-    registry.registerMetric(metric);
+    if (registry.getSingleMetric(metricName) === undefined)
+        registry.registerMetric(metric);
 
-  let runtime = "";
-  let node = "";
-  let engine = "";
-  try {
-    node = await api.rpc.system.version();
-    const getRuntime = await api.query.system.lastRuntimeUpgrade();
-    runtime = getRuntime.toJSON().specVersion
+    let runtime = "";
+    let node = "";
+    try {
+        node = await api.rpc.system.version();
+        const getRuntime = await api.query.system.lastRuntimeUpgrade();
+        runtime = getRuntime.toJSON().specVersion
 
-    metric.set({runtime, node}, 1)
+        metric.set({runtime, node}, 1)
 
 
-  } catch (e) {
-    logger.error(e);
-  }
+    } catch (e) {
+        logger.error(e);
+    }
 };
