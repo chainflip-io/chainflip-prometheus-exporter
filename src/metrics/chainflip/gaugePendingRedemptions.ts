8dd47ac44d6e4a55db2f3a8bb0ada5e8e743c273
marcellorigotti <marcello@chainflip.io>
2023-12-06 16:14:43 +0100

    fixed naming

--- src/metrics/chainflip/gaugePendingRedemptions.ts
+++ src/metrics/chainflip/gaugePendingRedemptions.ts
@@ -1,33 +1,33 @@
 import promClient, {Gauge} from "prom-client";
 import {Context} from "../../lib/interfaces";
 
-const metricPendingRedemptionName: string = "cf_pending_redemptions"
+const metricNamePendingRedemption: string = "cf_pending_redemptions"
 const metricPendingRedemption: Gauge = new promClient.Gauge({
-    name: metricPendingRedemptionName,
-    help: "Pending redemptions registered on the state-chain.",
+    name: metricNamePendingRedemption,
+    help: "The number of pending redemptions registered on the state-chain.",
     registers: [],
 });
 
-const metricPendingRedemptionBalanceName: string = "cf_pending_redemptions_balance"
+const metricNamePendingRedemptionBalance: string = "cf_pending_redemptions_balance"
 const metricPendingRedemptionBalance: Gauge = new promClient.Gauge({
-    name: metricPendingRedemptionBalanceName,
-    help: "The total balance of flip across all the redemption currently pending",
+    name: metricNamePendingRedemptionBalance,
+    help: "The total balance of flip across all the redemptions currently pending",
     registers: [],
 });
 
 export const gaugePendingRedemptions = async (context: Context): Promise<void> => {
     const {logger, api, registry, metricFailure} = context;
 
-    if (registry.getSingleMetric(metricPendingRedemptionName) === undefined)
+    if (registry.getSingleMetric(metricNamePendingRedemption) === undefined)
         registry.registerMetric(metricPendingRedemption);
-    if (registry.getSingleMetric(metricPendingRedemptionBalanceName) === undefined)
+    if (registry.getSingleMetric(metricNamePendingRedemptionBalance) === undefined)
         registry.registerMetric(metricPendingRedemptionBalance);
 
-    metricFailure.labels({metric: metricPendingRedemptionName}).set(0);
-    metricFailure.labels({metric: metricPendingRedemptionBalanceName}).set(0);
+    metricFailure.labels({metric: metricNamePendingRedemption}).set(0);
+    metricFailure.labels({metric: metricNamePendingRedemptionBalance}).set(0);
 
 
-    logger.debug(`Scraping ${metricPendingRedemptionName}, ${metricPendingRedemptionBalanceName}`);
+    logger.debug(`Scraping ${metricNamePendingRedemption}, ${metricNamePendingRedemptionBalance}`);
     try {
         const pendingRedemptions = await api.query.funding.pendingRedemptions.entries();
         let totalRedemptionBalance: number = 0;
@@ -39,7 +39,7 @@ export const gaugePendingRedemptions = async (context: Context): Promise<void> =
         metricPendingRedemption.set(pendingRedemptions.length);
     } catch (e) {
         logger.error(e);
-        metricFailure.labels({metric: metricPendingRedemptionName}).set(1);
-        metricFailure.labels({metric: metricPendingRedemptionBalanceName}).set(1);
+        metricFailure.labels({metric: metricNamePendingRedemption}).set(1);
+        metricFailure.labels({metric: metricNamePendingRedemptionBalance}).set(1);
     }
 };
