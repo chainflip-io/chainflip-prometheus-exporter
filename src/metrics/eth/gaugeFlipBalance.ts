91d43d43f6c3d25fc764ac1ccf039974f53f4fc4
Marcello <marcello@chainflip.io>
2024-03-20 13:24:00 +0100

    add FLIP totalSupply, queried from flip contract (#39)

---
+++ src/metrics/eth/gaugeFlipBalance.ts
@@ -0,0 +1,33 @@
+import promClient from 'prom-client';
+import { Contract, ethers } from 'ethers';
+import { Context } from '../../lib/interfaces';
+import { EthConfig } from '../../config/interfaces';
+
+const metricName: string = 'eth_flip_total_supply';
+const metric = new promClient.Gauge({
+    name: metricName,
+    help: 'The FLIP total supply register on the FLIP contract',
+    registers: [],
+});
+
+export const gaugeFlipBalance = async (context: Context) => {
+    const { logger, registry, metricFailure } = context;
+    const config = context.config as EthConfig;
+    const contract = context.contract as Contract;
+    try {
+        await contract.deployed();
+
+        logger.debug(`Scraping ${metricName}`);
+
+        if (registry.getSingleMetric(metricName) === undefined) registry.registerMetric(metric);
+
+        const totalSupply = await contract.totalSupply();
+        const metricValue: number = Number(Number(totalSupply) / 10 ** 18);
+
+        metric.set(metricValue);
+        metricFailure.labels({ metric: metricName }).set(0);
+    } catch (error) {
+        logger.error(error);
+        metricFailure.labels({ metric: metricName }).set(1);
+    }
+};
