f2fdaf35b90f6210205522fa7e8d816b39b37525
marcellorigotti <marcello@chainflip.io>
2024-03-26 16:34:47 +0100

    use processor to gather open channels info

--- src/config/getConfig.ts
+++ src/config/getConfig.ts
@@ -26,6 +26,7 @@ const ETH_WS_ENDPOINT: string = process.env.ETH_WS_ENDPOINT || 'ws://localhost:8
 
 const CACHE_ENDPOINT: string = process.env.CACHE_ENDPOINT || '';
 
+const PROCESSOR_ENDPOINT: string = process.env.PROCESSOR_ENDPOINT || '';
 export interface Env {
     CONFIG_PATH: string;
     BTC_HTTP_ENDPOINT: string;
@@ -40,6 +41,7 @@ export interface Env {
     DOT_WS_ENDPOINT: string;
     ETH_WS_ENDPOINT: string;
     CACHE_ENDPOINT: string;
+    PROCESSOR_ENDPOINT: string;
 }
 
 export const env: Env = {
@@ -56,6 +58,7 @@ export const env: Env = {
     DOT_WS_ENDPOINT,
     ETH_WS_ENDPOINT,
     CACHE_ENDPOINT,
+    PROCESSOR_ENDPOINT,
 };
 
 export default function getConfig(logger: Logger): any {
--- src/metrics/chainflip/gaugeDepositChannels.ts
+++ src/metrics/chainflip/gaugeDepositChannels.ts
@@ -1,13 +1,25 @@
 import promClient, { Gauge } from 'prom-client';
 import { Context } from '../../lib/interfaces';
 import { FlipConfig } from '../../config/interfaces';
+import { Axios } from 'axios';
+import { env } from '../../config/getConfig';
 
 const metricName: string = 'cf_open_deposit_channels';
 const metric: Gauge = new promClient.Gauge({
     name: metricName,
     help: 'The number of open deposit channels',
-    labelNames: ["deposit_chain"],
+    labelNames: ['deposit_chain'],
     registers: [],
+});
+
+const axios = new Axios({
+    baseURL: env.PROCESSOR_ENDPOINT,
+    timeout: 6000,
+    headers: {
+        accept: 'application/json',
+        'cache-control': 'no-cache',
+        'content-type': 'application/json',
+    },
 });
 
 export const gaugeDepositChannels = async (context: Context): Promise<void> => {
@@ -21,54 +33,33 @@ export const gaugeDepositChannels = async (context: Context): Promise<void> => {
     metricFailure.labels({ metric: metricName }).set(0);
 
     try {
-        // BITCOIN CHANNELS
-        if (global.btcHeight) {
-            const channels = await api.query.bitcoinIngressEgress.depositChannelLookup.entries();
-            let numberOfChannels = 0;
-            channels.forEach(([key, element]: [any, any]) => {
-                let channel = element.toJSON();
-                if (channel.expiresAt >= global.btcHeight) {
-                    numberOfChannels++;
-                }
-            });
-            metric.labels("bitcoin").set(numberOfChannels);
-        }
+        const data = await axios.post(
+            env.PROCESSOR_ENDPOINT,
+            `{"query":"query GetOpenSwapChannels {\\n  channels: allSwapChannels(\\n    condition: {isExpired: false}\\n    orderBy: ISSUED_EVENT_ID_DESC\\n  ) {\\n    nodes {\\n      sourceChain\\n    }\\n  }\\n}"}`,
+        );
 
-        // The API is not able to correctly decode polkadot and ethereum IngressEgress depositChannelLookup
-        // ETHEREUM CHANNELS
-        // if (global.ethHeight) {
-        //     const channels = await api.query.ethereumIngressEgress.depositChannelLookup.entries();
-        //     let numberOfChannels = 0;
-        //     channels.forEach(([key, element]: [any, any]) => {
-        //         let channel = element.toJSON();
-        //         console.log(channel)
-        //         console.log("\n\n");
-        //         if (channel.expiresAt >= global.ethHeight) {
-        //             numberOfChannels++;
-        //         }
-        //         // totalRedemptionBalance += parseFloat(element.toHuman().total.replace(/,/g, '')) / 1e18;
-        //     });
-        //     console.log(numberOfChannels);
-        //     metric.labels("ethereum").set(numberOfChannels);
-        // }
+        const channels = JSON.parse(data.data).data.channels.nodes;
+        let ethChannels = 0;
+        let btcChannels = 0;
+        let dotChannels = 0;
+        channels.forEach((channel) => {
+            switch (channel.sourceChain) {
+                case 'Ethereum':
+                    ethChannels++;
+                    break;
+                case 'Bitcoin':
+                    btcChannels++;
+                    break;
+                case 'Polkadot':
+                    dotChannels++;
+                    break;
+            }
+        });
+        metric.labels('bitcoin').set(btcChannels);
 
-        // POLKADOT CHANNELS
-        // if (global.dotHeight) {
-        //     const channels = await api.query.polkadotIngressEgress.depositChannelLookup.entries();
-        //     let numberOfChannels = 0;
-        //     channels.forEach(([key, element]: [any, any]) => {
-        //         let channel = element.toJSON();
-        //         console.log(channel)
-        //         console.log("\n\n");
-        //         if (channel.expiresAt >= global.dotHeight) {
-        //             numberOfChannels++;
-        //         }
-        //         // totalRedemptionBalance += parseFloat(element.toHuman().total.replace(/,/g, '')) / 1e18;
-        //     });
-        //     console.log(numberOfChannels);
-        //     metric.labels("polkadot").set(numberOfChannels);
-        // }
+        metric.labels('polkadot').set(dotChannels);
 
+        metric.labels('ethereum').set(ethChannels);
     } catch (e) {
         logger.error(e);
         metricFailure.labels({ metric: metricName }).set(1);
