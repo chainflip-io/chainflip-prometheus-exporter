06d20a99c63ad760b55279fcfb65acb554fdcbf3
marcellorigotti <marcello@chainflip.io>
2024-06-28 16:09:05 +0200

    ignore some metrics on localnet/sisyphos

--- src/config/getConfig.ts
+++ src/config/getConfig.ts
@@ -86,6 +86,7 @@ export default function getConfig(logger: Logger): any {
         return config;
     } catch (err) {
         logger.error('Error reading production config', err);
+        process.exit(1);
     }
     return;
 }
