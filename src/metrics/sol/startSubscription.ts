aaa3ccf5edb64539c513b43fe4673423ea2fa885
marcellorigotti <marcello@chainflip.io>
2025-04-03 17:08:54 +0200

    decode instruction

--- src/metrics/sol/startSubscription.ts
+++ src/metrics/sol/startSubscription.ts
@@ -1,6 +1,7 @@
 import promClient, { Gauge } from 'prom-client';
 import { Context } from '../../lib/interfaces';
 import { PublicKey, NonceAccount } from '@solana/web3.js';
+import base58 from 'bs58';
 
 const metricSolanaTxRevertedName: string = 'sol_tx_reverted';
 const metricSolanaTxReverted: Gauge = new promClient.Gauge({
@@ -39,8 +40,12 @@ export const startSubscription = async (context: Context) => {
                 if (log.err !== null) {
                     const transaction = await context.connection.getTransaction(log.signature, {
                         commitment: 'finalized',
+                        maxSupportedTransactionVersion: 0,
                     });
+                    //4t5EHJWtGACTXyzT5YC8LDwAuK5iqgXckJz7gaKGBwKkcfuKT1UmCfGZdTVT8GX62oi4xDt9ZXTLbV4t91txdj1k
                     const keys = transaction.transaction.message.accountKeys;
+                    const instruction = transaction.transaction.message.instructions[4].data;
+                    const decoded_instruction = base58.decode(instruction)
                     // only report reverted tx if they are originated from our aggKey
                     if (keys[0].toString() === global.solanaCurrentOnChainKey) {
                         metricSolanaTxReverted.labels(log.signature).set(1);
