b13121cd95b51b9d0432853366699b3e929c7a30
Marcello <marcello@chainflip.io>
2025-04-08 14:14:27 +0200

    feat: create separate metric for CCM solana tx reverted (#101)
    * decode instruction
    
    * separate CCM tx from normal tx
    
    * reduce liveness to 1m
    
    * add check for number of instructions
    
    * add else condition to capture rotation/batched fetch txs
    
    * fix: remove entry from metricSolanaCCMTxReverted

--- src/metrics/sol/startSubscription.ts
+++ src/metrics/sol/startSubscription.ts
@@ -1,6 +1,7 @@
 import promClient, { Gauge } from 'prom-client';
 import { Context } from '../../lib/interfaces';
 import { PublicKey, NonceAccount } from '@solana/web3.js';
+import base58 from 'bs58';
 
 const metricSolanaTxRevertedName: string = 'sol_tx_reverted';
 const metricSolanaTxReverted: Gauge = new promClient.Gauge({
@@ -10,8 +11,20 @@ const metricSolanaTxReverted: Gauge = new promClient.Gauge({
     labelNames: ['txHash'],
 });
 
+const metricSolanaCCMTxRevertedName: string = 'sol_ccm_tx_reverted';
+const metricSolanaCCMTxReverted: Gauge = new promClient.Gauge({
+    name: metricSolanaCCMTxRevertedName,
+    help: 'If a ccm tx reverted on-chain',
+    registers: [],
+    labelNames: ['txHash'],
+});
+
 let lastOnChainKey = '';
 let subscriptionId: number;
+
+// ExecuteCcmNative => 7d050be38042e0b2
+// ExecuteCcmToken => 6cb8a27b9fdea223
+const ccmInstructions = ['7d050be38042e0b2', '6cb8a27b9fdea223'];
 
 export const startSubscription = async (context: Context) => {
     if (context.config.skipMetrics.includes('sol_subscriptions')) {
@@ -29,6 +42,8 @@ export const startSubscription = async (context: Context) => {
 
     if (registry.getSingleMetric(metricSolanaTxRevertedName) === undefined)
         registry.registerMetric(metricSolanaTxReverted);
+    if (registry.getSingleMetric(metricSolanaCCMTxRevertedName) === undefined)
+        registry.registerMetric(metricSolanaCCMTxReverted);
 
     metricFailure.labels({ metric: metricSolanaTxRevertedName }).set(0);
 
@@ -39,14 +54,37 @@ export const startSubscription = async (context: Context) => {
                 if (log.err !== null) {
                     const transaction = await context.connection.getTransaction(log.signature, {
                         commitment: 'finalized',
+                        maxSupportedTransactionVersion: 0,
                     });
                     const keys = transaction.transaction.message.accountKeys;
                     // only report reverted tx if they are originated from our aggKey
                     if (keys[0].toString() === global.solanaCurrentOnChainKey) {
-                        metricSolanaTxReverted.labels(log.signature).set(1);
-                        setTimeout(() => {
-                            metricSolanaTxReverted.remove(log.signature);
-                        }, 60000); // 1m
+                        if (transaction.transaction.message.instructions.length <= 4) {
+                            // Transfer/fetch
+                            metricSolanaTxReverted.labels(log.signature).set(1);
+                            setTimeout(() => {
+                                metricSolanaTxReverted.remove(log.signature);
+                            }, 60000); // 1m
+                        } else {
+                            const instruction =
+                                transaction.transaction.message.instructions[4].data;
+                            const decoded_instruction = Buffer.from(
+                                base58.decode(instruction).slice(0, 8),
+                            ).toString('hex');
+                            if (ccmInstructions.includes(decoded_instruction)) {
+                                // CCM
+                                metricSolanaCCMTxReverted.labels(log.signature).set(1);
+                                setTimeout(() => {
+                                    metricSolanaCCMTxReverted.remove(log.signature);
+                                }, 60000); // 1m
+                            } else {
+                                // Rotation/Batched fetches
+                                metricSolanaTxReverted.labels(log.signature).set(1);
+                                setTimeout(() => {
+                                    metricSolanaTxReverted.remove(log.signature);
+                                }, 60000); // 1m
+                            }
+                        }
                     }
                 }
             },
