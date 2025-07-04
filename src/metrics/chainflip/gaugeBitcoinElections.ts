c2459809efc2c2db87dd0d5e412c916b9acb8ed3
Marcello <marcello@chainflip.io>
2025-07-04 16:35:15 +0200

    add extra metrics (#109)

--- src/metrics/chainflip/gaugeBitcoinElections.ts
+++ src/metrics/chainflip/gaugeBitcoinElections.ts
@@ -26,6 +26,30 @@ const metricHighestEverOngoing: Gauge = new promClient.Gauge({
     registers: [],
 });
 
+const metricNameQueuedHash: string = 'cf_bw_queued_hash_elections';
+const metricQueuedHash: Gauge = new promClient.Gauge({
+    name: metricNameQueuedHash,
+    help: 'BW number of queued hash elections',
+    labelNames: ['tracked_chain', 'bw_instance'],
+    registers: [],
+});
+
+const metricNameQueuedSafe: string = 'cf_bw_queued_safe_elections';
+const metricQueuedSafe: Gauge = new promClient.Gauge({
+    name: metricNameQueuedSafe,
+    help: 'BW number of queued safe elections',
+    labelNames: ['tracked_chain', 'bw_instance'],
+    registers: [],
+});
+
+const metricNameOngoing: string = 'cf_bw_ongoing_elections';
+const metricOngoing: Gauge = new promClient.Gauge({
+    name: metricNameOngoing,
+    help: 'BW number of ongoing elections',
+    labelNames: ['tracked_chain', 'bw_instance'],
+    registers: [],
+});
+
 export const gaugeBitcoinElections = async (
     context: Context,
     data: ProtocolData,
@@ -44,6 +68,15 @@ export const gaugeBitcoinElections = async (
     if (registry.getSingleMetric(metricNameHighestEverOngoing) === undefined) {
         registry.registerMetric(metricHighestEverOngoing);
     }
+    if (registry.getSingleMetric(metricNameQueuedHash) === undefined) {
+        registry.registerMetric(metricQueuedHash);
+    }
+    if (registry.getSingleMetric(metricNameQueuedSafe) === undefined) {
+        registry.registerMetric(metricQueuedSafe);
+    }
+    if (registry.getSingleMetric(metricNameOngoing) === undefined) {
+        registry.registerMetric(metricOngoing);
+    }
 
     try {
         const api = await apiLatest.at(data.blockHash);
@@ -54,65 +87,102 @@ export const gaugeBitcoinElections = async (
         const bhw_witnessFrom = unsync_state[0].phase.runningBitcoin.witnessFrom;
         metricWitnessFrom.labels('bitcoin').set(bhw_witnessFrom);
 
+        // Block Height Witnesser
         logger.info('Bitcoin_BHW_state', {
             block: data.header,
             message_bhw_witnessFrom: bhw_witnessFrom,
             message_bhw_headers: unsync_state[0].phase.runningBitcoin.headers,
         });
 
+        // Deposit channels
         const bw_deposit_channels = unsync_state[1];
         const bw_deposit_channels_seen_heights_below =
             bw_deposit_channels.elections.seenHeightsBelow;
         const bw_deposit_channels_highest_ever_ongoing =
             bw_deposit_channels.elections.highestEverOngoingElection;
+        const bw_deposit_channels_queued_hash = bw_deposit_channels.elections.queuedHashElections;
+        const bw_deposit_channels_queued_safe =
+            bw_deposit_channels.elections.queuedSafeElections.elections;
+        const bw_deposit_channels_ongoing = bw_deposit_channels.elections.ongoing;
         metricSeenHeightsBelow
             .labels('bitcoin', 'deposit_channels')
             .set(bw_deposit_channels_seen_heights_below);
         metricHighestEverOngoing
             .labels('bitcoin', 'deposit_channels')
             .set(bw_deposit_channels_highest_ever_ongoing);
-
-        const bw_vaults = unsync_state[2];
-        const bw_vaults_seen_heights_below = bw_vaults.elections.seenHeightsBelow;
-        const bw_vaults_highest_ever_ongoing = bw_vaults.elections.highestEverOngoingElection;
-        metricSeenHeightsBelow.labels('bitcoin', 'vaults').set(bw_vaults_seen_heights_below);
-        metricHighestEverOngoing.labels('bitcoin', 'vaults').set(bw_vaults_highest_ever_ongoing);
-
-        const bw_egresses = unsync_state[3];
-        const bw_egresses_seen_heights_below = bw_egresses.elections.seenHeightsBelow;
-        const bw_egresses_highest_ever_ongoing = bw_egresses.elections.highestEverOngoingElection;
-        metricSeenHeightsBelow.labels('bitcoin', 'egresses').set(bw_egresses_seen_heights_below);
-        metricHighestEverOngoing
-            .labels('bitcoin', 'egresses')
-            .set(bw_egresses_highest_ever_ongoing);
+        metricQueuedHash
+            .labels('bitcoin', 'deposit_channels')
+            .set(Object.keys(bw_deposit_channels_queued_hash).length);
+        metricQueuedSafe
+            .labels('bitcoin', 'deposit_channels')
+            .set(Object.keys(bw_deposit_channels_queued_safe).length);
+        metricOngoing
+            .labels('bitcoin', 'deposit_channels')
+            .set(Object.keys(bw_deposit_channels_ongoing).length);
         logger.info('Bitcoin_BW_deposit_channels_state', {
             block: data.header,
             data: {
                 bw_deposit_channels_seen_heights_below,
                 bw_deposit_channels_highest_ever_ongoing,
-                bw_deposit_channels_ongoing: bw_deposit_channels.elections.ongoing,
+                bw_deposit_channels_ongoing,
+                bw_deposit_channels_queued_safe,
+                bw_deposit_channels_queued_hash,
                 bw_blocks_data: bw_deposit_channels.blockProcessor.blocksData,
                 bw_processed_events: bw_deposit_channels.blockProcessor.processedEvents,
             },
         });
 
+        // Vaults
+        const bw_vaults = unsync_state[2];
+        const bw_vaults_seen_heights_below = bw_vaults.elections.seenHeightsBelow;
+        const bw_vaults_highest_ever_ongoing = bw_vaults.elections.highestEverOngoingElection;
+        const bw_vaults_queued_hash = bw_vaults.elections.queuedHashElections;
+        const bw_vaults_queued_safe = bw_vaults.elections.queuedSafeElections.elections;
+        const bw_vaults_ongoing = bw_vaults.elections.ongoing;
+        metricSeenHeightsBelow.labels('bitcoin', 'vaults').set(bw_vaults_seen_heights_below);
+        metricHighestEverOngoing.labels('bitcoin', 'vaults').set(bw_vaults_highest_ever_ongoing);
+        metricQueuedHash.labels('bitcoin', 'vaults').set(Object.keys(bw_vaults_queued_hash).length);
+        metricQueuedSafe.labels('bitcoin', 'vaults').set(Object.keys(bw_vaults_queued_safe).length);
+        metricOngoing.labels('bitcoin', 'vaults').set(Object.keys(bw_vaults_ongoing).length);
         logger.info('Bitcoin_BW_vaults_state', {
             block: data.header,
             data: {
                 bw_vaults_seen_heights_below,
                 bw_vaults_highest_ever_ongoing,
-                bw_vaults_ongoing: bw_vaults.elections.ongoing,
+                bw_vaults_ongoing,
+                bw_vaults_queued_safe,
+                bw_vaults_queued_hash,
                 bw_blocks_data: bw_vaults.blockProcessor.blocksData,
                 bw_processed_events: bw_vaults.blockProcessor.processedEvents,
             },
         });
 
+        // Egresses
+        const bw_egresses = unsync_state[3];
+        const bw_egresses_seen_heights_below = bw_egresses.elections.seenHeightsBelow;
+        const bw_egresses_highest_ever_ongoing = bw_egresses.elections.highestEverOngoingElection;
+        const bw_egresses_queued_hash = bw_egresses.elections.queuedHashElections;
+        const bw_egresses_queued_safe = bw_egresses.elections.queuedSafeElections.elections;
+        const bw_egresses_ongoing = bw_egresses.elections.ongoing;
+        metricSeenHeightsBelow.labels('bitcoin', 'egresses').set(bw_egresses_seen_heights_below);
+        metricHighestEverOngoing
+            .labels('bitcoin', 'egresses')
+            .set(bw_egresses_highest_ever_ongoing);
+        metricQueuedHash
+            .labels('bitcoin', 'egresses')
+            .set(Object.keys(bw_egresses_queued_hash).length);
+        metricQueuedSafe
+            .labels('bitcoin', 'egresses')
+            .set(Object.keys(bw_egresses_queued_safe).length);
+        metricOngoing.labels('bitcoin', 'egresses').set(Object.keys(bw_egresses_ongoing).length);
         logger.info('Bitcoin_BW_egresses_state', {
             block: data.header,
             data: {
                 bw_egresses_seen_heights_below,
                 bw_egresses_highest_ever_ongoing,
-                bw_egresses_ongoing: bw_egresses.elections.ongoing,
+                bw_egresses_ongoing,
+                bw_egresses_queued_safe,
+                bw_egresses_queued_hash,
                 bw_blocks_data: bw_egresses.blockProcessor.blocksData,
                 bw_processed_events: bw_egresses.blockProcessor.processedEvents,
             },
