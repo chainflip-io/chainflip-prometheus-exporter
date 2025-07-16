ed9b4b57c21b7c1e76d11135d59db1b4754ccf6a
marcellorigotti <marcello@chainflip.io>
2025-07-16 18:24:04 +0200

    add cf_open_elections for btc

--- src/metrics/chainflip/gaugeOpenElections.ts
+++ src/metrics/chainflip/gaugeOpenElections.ts
@@ -10,25 +10,44 @@ const metricOpenElection: Gauge = new promClient.Gauge({
     registers: [],
 });
 
-const map = new Map([
+const mapSolana = new Map([
     ['A', 'SolanaBlockHeightTracking'],
     ['B', 'SolanaIngressTracking'],
     ['C', 'SolanaNonceTracking'],
     ['D', 'SolanaEgressWitnessing'],
     ['EE', 'SolanaLiveness'],
     ['FF', 'SolanaVaultSwapTracking'],
+    ['G', 'SolanaAltWitnessing'],
 ]);
 
-type election_count = {
+const mapBitcoin = new Map([
+    ['A', 'BitcoinBlockHeightWitnesserES'],
+    ['B', 'BitcoinDepositChannelWitnessingES'],
+    ['C', 'BitcoinVaultDepositWitnessingES'],
+    ['D', 'BitcoinEgressWitnessingES'],
+    ['EE', 'BitcoinFeeTracking'],
+    ['FF', 'BitcoinLiveness'],
+]);
+
+type election_count_sol = {
     A: number;
     B: number;
     C: number;
     D: number;
     EE: number;
     FF: number;
+    G: number;
+};
+type election_count_btc = {
+    A: number;
+    B: number;
+    C: number;
+    D: number;
+    EE: number;
+    FF: number; 
 };
 export const gaugeOpenElections = async (context: Context, data: ProtocolData): Promise<void> => {
-    if (context.config.skipMetrics.includes('cf_validator')) {
+    if (context.config.skipMetrics.includes('cf_open_elections')) {
         return;
     }
     const { logger, apiLatest, registry, metricFailure } = context;
@@ -39,7 +58,16 @@ export const gaugeOpenElections = async (context: Context, data: ProtocolData): 
         registry.registerMetric(metricOpenElection);
 
     metricFailure.labels({ metric: metricNameOpenElection }).set(0);
-    const count: election_count = {
+    const countSolana: election_count_sol = {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0,
+        EE: 0,
+        FF: 0,
+        G: 0,
+    };
+    const countBitcoin: election_count_btc = {
         A: 0,
         B: 0,
         C: 0,
@@ -47,7 +75,6 @@ export const gaugeOpenElections = async (context: Context, data: ProtocolData): 
         EE: 0,
         FF: 0,
     };
-
     try {
         const api = await apiLatest.at(data.blockHash);
         const result = await api.query.solanaElections.electionProperties.entries();
@@ -56,17 +83,38 @@ export const gaugeOpenElections = async (context: Context, data: ProtocolData): 
             if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
                 const electoral_system = Object.keys(
                     value,
-                )[0].toUpperCase() as keyof election_count;
-                count[electoral_system] = count[electoral_system] + 1;
+                )[0].toUpperCase() as keyof election_count_sol;
+                countSolana[electoral_system] = countSolana[electoral_system] + 1;
             } else if (value !== null) {
-                count[value.toUpperCase() as keyof election_count] =
-                    count[value.toUpperCase() as keyof election_count] + 1;
+                countSolana[value.toUpperCase() as keyof election_count_sol] =
+                countSolana[value.toUpperCase() as keyof election_count_sol] + 1;
             }
         });
 
-        for (const [key, value] of Object.entries(count)) {
-            const full_name = map.get(key) as string;
+        for (const [key, value] of Object.entries(countSolana)) {
+            const full_name = mapSolana.get(key) as string;
             metricOpenElection.labels('solana', key.concat('_', full_name)).set(value);
+        }
+
+
+
+        const result2 = await api.query.bitcoinElections.electionProperties.entries();
+        result2.forEach(([_, election_properties]: any[]) => {
+            const value = election_properties.toJSON();
+            if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
+                const electoral_system = Object.keys(
+                    value,
+                )[0].toUpperCase() as keyof election_count_btc;
+                countBitcoin[electoral_system] = countBitcoin[electoral_system] + 1;
+            } else if (value !== null) {
+                countBitcoin[value.toUpperCase() as keyof election_count_btc] =
+                countBitcoin[value.toUpperCase() as keyof election_count_btc] + 1;
+            }
+        });
+
+        for (const [key, value] of Object.entries(countBitcoin)) {
+            const full_name = mapBitcoin.get(key) as string;
+            metricOpenElection.labels('bitcoin', key.concat('_', full_name)).set(value);
         }
     } catch (e) {
         metricFailure.labels({ metric: metricNameOpenElection }).set(1);
